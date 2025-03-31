fc4ba3f847cf42b52fd0515d9f512d290b9f5a90
Michael Afari <michealnewtons419@gmail.com>
2025-03-31 12:59:34 +0200

    done locally

--- js/script.js
+++ js/script.js
@@ -71,7 +71,10 @@ if ("serviceWorker" in navigator) {
     navigator.serviceWorker
       .register("/sw.js")
       .then((registration) => {
-        console.log("ServiceWorker registration successful");
+        console.log(
+          "ServiceWorker registration successful",
+          registration.scope
+        );
       })
       .catch((err) => {
         console.log("ServiceWorker registration failed: ", err);
